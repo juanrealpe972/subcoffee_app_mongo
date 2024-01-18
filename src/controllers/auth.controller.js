21e00adca3ca9dd4389aaf530c4e3fe7aa356b93
Juank <juanrealpe972@gmail.com>
2024-01-17 23:01:34 -0500

    add funcionalidad al registro y creando token cada que se crea un user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,33 @@
 import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs'
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
     const { email, password, username } = req.body;
     try {
+
+        const passwordHash = await bcrypt.hash(password, 10)
         const newUser = new User({
             username,
             email,
-            password
+            password: passwordHash
         })
-        await newUser.save();
-        res.send("Registrando")
+
+        const userSaved = await newUser.save();
+        const token = await createAccessToken({ id: userSaved._id })
+        // return json web token
+        res.cookie('token', token);
+
+        res.json({
+            id: userSaved.id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt
+        })
+
     } catch (error) {
-        console.log(error);
+        res.status(500).json({ message: error.message });
     }
 }
 export const login = (req, res) => res.send("login")
