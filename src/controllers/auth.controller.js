4d2ef40c7b0f22a123ff146fc5d6da86f70240fa
Juank <juanrealpe972@gmail.com>
2024-01-18 00:15:10 -0500

    add funciones de autorizacion de token y protegiendo rutas

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -64,3 +64,18 @@ export const logout = (req, res) => {
     });
     return res.sendStatus(200)
 }
+
+export const profile = async (req, res) => {
+    const userFound = await User.findById(req.user.id)
+
+    if (!userFound) return res.status(400).json({ message: "User not found" })
+
+    return res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.updatedAt
+    })
+    res.send('profile')
+}
