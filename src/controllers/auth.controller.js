953ecfa60a9859a7632dab9fb964d6c5bf16f635
Juank <juanrealpe972@gmail.com>
2024-01-17 23:43:45 -0500

    add funcinalidad al login y al logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -30,4 +30,37 @@ export const register = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
-export const login = (req, res) => res.send("login")
+
+export const login = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        const userFound = await User.findOne({ email })
+        if (!userFound) return res.status(400).json({ message: "User not found" })
+
+        const isMatch = await bcrypt.compare(password, userFound.password)
+        if (!isMatch) return res.status(400).json({ message: "Incorrect password" })
+
+        const token = await createAccessToken({ id: userFound._id })
+        // return json web token
+        res.cookie('token', token);
+        res.json({
+            id: userFound.id,
+            username: userFound.username,
+            email: userFound.email,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const logout = (req, res) => {
+    res.cookie("token", "", {
+        expires: new Date(0)
+    });
+    return res.sendStatus(200)
+}
