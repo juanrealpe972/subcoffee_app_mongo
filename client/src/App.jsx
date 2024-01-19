6801b17cfa4e62d7f1f20b98149f658866724d8c
Juank <juanrealpe972@gmail.com>
2024-01-18 22:46:44 -0500

    add rotas e instalando react-router-dom para facilitar el acceso a most page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold ">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/tasks" element={<h1>Tasks Page</h1>} />
+        <Route path="/add-task" element={<h1>Create task</h1>} />
+        <Route path="/tasks/:id" element={<h1>Update task</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
